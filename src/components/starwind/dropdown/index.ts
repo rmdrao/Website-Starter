e5bc4687d47e21089912400039b6dcccf46413a6
Boston343 <branden.austin1@gmail.com>
2025-05-03 03:47:39 -0400

    improve hover dropdowns

--- src/components/starwind/dropdown/index.ts
+++ src/components/starwind/dropdown/index.ts
@@ -1,17 +1,17 @@
 import Dropdown from "./Dropdown.astro";
+import DropdownTrigger from "./DropdownTrigger.astro";
 import DropdownContent from "./DropdownContent.astro";
 import DropdownItem from "./DropdownItem.astro";
 import DropdownLabel from "./DropdownLabel.astro";
 import DropdownSeparator from "./DropdownSeparator.astro";
-import DropdownTrigger from "./DropdownTrigger.astro";
 
 export {
 	Dropdown,
+	DropdownTrigger,
 	DropdownContent,
 	DropdownItem,
 	DropdownLabel,
 	DropdownSeparator,
-	DropdownTrigger,
 };
 
 export default {
